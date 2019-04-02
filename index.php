0e8ca70b7769b84f0ca78fdbcd8ba1abf14a47f6
Federico <fserenelli@gmail.com>
2019-04-02 18:45:54 -0300

    comienzo página de loging

--- index.php
+++ index.php
@@ -1,93 +1,46 @@
 <!DOCTYPE html>
 <html>
-  <head>
+  <head> 
     <meta charset="utf-8">
-    <title>Aprendiendo PHP</title>
+    <title>Cursos Online</title>
     <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
-    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    <div class="contenedor">
-      <h1>Aprendiendo PHP</h1>
-        <div class="contenido">
-          <form class="" action="respuesta.php" method="post">
-            <div class="informacion">
-                  <div class="campo">
-                      <label for="nombre">Nombre
-                          <input type="text" name="nombre" id="nombre">
-                      </label>
-                  </div>
-                  <div class="campo">
-                      <label for="apellido">Apellido
-                          <input type="text" name="apellido" id="apellido">
-                      </label>
-                  </div>
-
-                  <!-- Desde un checkbox-->
-                  <div class="campo">
-                      <label for="notificaciones">Notificaciones
-                          <input type="checkbox" name="notificaciones" id="notificaciones">
-                      </label>
-                  </div>
-          </div> <!--.informacion-->
-
-                  <!-- Desde array de Checkboxes-->
-                  <div class="cursos">
-                            <h2>Cursos</h2>
-                            <div class="campo">
-                                <label for="html5">HTML5
-                                    <input type="checkbox" name="curso[]" value="html5" id="html5">
-                                </label>
-                            </div>
-                            <div class="campo">
-                                <label for="css3">CSS3
-                                    <input type="checkbox" name="curso[]" value="css3" id="css3">
-                                </label>
-                            </div>
-                            <div class="campo">
-                                <label for="javascript">JavaScipt
-                                    <input type="checkbox" name="curso[]" value="javascript" id="javascript">
-                                </label>
-                            </div>
-                  </div>
-                  <div class="especializacion">
-                    <h2>Área de Especialización</h2>
-
-                        <select name="area" value="-Any-">
-                            <option>- Selecciona una area -</option>
-                            <option value="fe">Front End</option>
-                            <option value="be">Back End</option>
-                            <option value="fs">Full Stack</option>
-                        </select>
-                  </div>
-            <div class="tipo_curso">
-                  <h2>Tipo de Curso</h2>
-                  <?php  $opciones = array(
-                      'pres' => 'Presencial',
-                      'online' => 'En Línea'
-                  );?>
-
-                  <?php foreach($opciones as $key => $opcion) {
-                    echo "<div class='campo'>";
-                    echo "<input type='radio' name='opciones' value='$key' > $opcion";
-                    echo "</div>";
-                  } ?>
+    <div class="contenedor-principal">
+      <div class="contenedor c-izquierda"></div>
+      <div class="contenedor c-derecha">
+        <form action="#" class="registro" method="post">
+          <div class="nombre-sitio">
+            <h1 class="nombre-sitio">Acceder</h1>
+          </div>
+          <div class="login">
+            <div>  
+              <div class="campo">
+                <label for="usuario">Usuario:</label>
+                <input type="text" id="usuario" name="usuario" placeholder="usuario@xxxx.com">
+              </div>
+              <div class="campo">
+                <label for="pass">Contraseña:</label>
+                <input type="text" id="pass" name="pass" placeholder="Contraseña">
+              </div>
+              <div class="forget-pass">
+                <a href="#">Forget password?</a>
+              </div>
             </div>
-
-            <div class="textarea">
-                  <div class="campo">
-                    <label for="mensaje"> Mensaje <br/>
-                        <textarea name="mensaje" rows="8" cols="40" id="mensaje"></textarea>
-                    </label>
-                    <input type="submit">
-                  </div>
-            </div>
-
-
-            
-            
-          </form>
-        </div>
+          </div>
+          <div class="botones">
+              <div class="iniciar-sesion">
+                <input type="submit" id="btnIniciarSesion" class="btnIniciarSesion" value="Siguiente">
+              </div>
+              <div class="crear-cuenta">
+                <input type="submit" id="btnCrearCuenta" class="btnCrearCuenta" value="Crear cuenta">
+              </div>
+          </div>
+        </form>
+      </div>
     </div>
   </body>
 </html>
